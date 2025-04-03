ad87420280db6cf2180f31bc893ecc8057029830
EssumanG <godsaves.essuman@amalitechtraining.org>
2025-04-03 18:45:07 +0000

    store the cleaned data

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,7 @@
 from utils import *
 
 raw_data_dest = './datasets/raw_data/'
+staged_processed_data_file = './datasets/preprocessed_dataset/stage_data.csv' 
 
 if __name__ =="__main__":
     download_data("sakshisatre/titanic-dataset", raw_data_dest)
@@ -9,4 +10,5 @@ if __name__ =="__main__":
     df = remove_duplactes(df)
     df = fill_null_values(df)
     df = encode_categories(df)
-    print(df.head(10))
+    store_preprocessed_data(df, staged_processed_data_file)
+    # print(df.head(10))
