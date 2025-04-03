3aacdb3c7553381b6682245f2f9851b456b0b97a
GEssuman <godsaves.essuman@amalitechtraining.org>
2025-04-03 19:27:26 +0000

    Add files via upload

---
+++ preprocess_data.py
@@ -0,0 +1,14 @@
+from utils import *
+
+raw_data_dest = './datasets/raw_data/'
+staged_processed_data_file = './datasets/preprocessed_dataset/stage_data.csv' 
+
+if __name__ =="__main__":
+    download_data("sakshisatre/titanic-dataset", raw_data_dest)
+    df = read_csv_file('./datasets/raw_data/The Titanic dataset.csv')
+    df = rename_column(df)
+    df = remove_duplactes(df)
+    df = fill_null_values(df)
+    df = encode_categories(df)
+    store_preprocessed_data(df, staged_processed_data_file)
+    # print(df.head(10))
