534b88e4541b5c2e3c74d4c16cb6912e9058312e
EssumanG <godsaves.essuman@amalitechtraining.org>
2025-04-04 00:40:46 +0000

    modified the preprocessed_data file to clean data only

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,14 +1,16 @@
 from utils import *
 
-raw_data_dest = './datasets/raw_data/'
-staged_processed_data_file = './datasets/preprocessed_dataset/stage_data.csv' 
 
-if __name__ =="__main__":
-    download_data("sakshisatre/titanic-dataset", raw_data_dest)
-    df = read_csv_file('./datasets/raw_data/The Titanic dataset.csv')
+def preprocess_data(df):
+    print("Cleaning and preprocessing data...")
     df = rename_column(df)
     df = remove_duplactes(df)
     df = fill_null_values(df)
     df = encode_categories(df)
-    store_preprocessed_data(df, staged_processed_data_file)
-    # print(df.head(10))
+    return df
+
+if __name__ =="__main__":
+    RAW_DATA_FILE = './datasets/raw_data/The Titanic dataset.csv'
+    df = read_csv_file(RAW_DATA_FILE)
+    preprocess_data(df)
+    print(df.head(10))
